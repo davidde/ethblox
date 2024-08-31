351cb60ef4afbf2bd00115090d9a81b0d0db337e
David Deprost <dadeprost@gmail.com>
2024-08-31 20:03:25 +0200

    Add hover effects to icons

--- src/components/toggle-icons.tsx
+++ src/components/toggle-icons.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 export default function ToggleIcons() {
   return (
     <span className='flex flex-row justify-end'>
-      <DarkModeToggle className='w-6 cursor-pointer' />
+      <DarkModeToggle className='w-6 cursor-pointer hover:text-sky-300' />
       <Image
           src='/ethereum-logo-light.svg'
           width={16}
           height={16}
           alt='Ethereum Logo'
-          className='ml-5 cursor-pointer'
+          className='ml-5 cursor-pointer hover:invert-[0.65]'
       />
     </span>
   );
