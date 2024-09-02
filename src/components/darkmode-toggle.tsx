87015b9a0c3c23370f8865aa4203ee8d43672086
David Deprost <dadeprost@gmail.com>
2024-09-02 12:45:12 +0200

    Persist dark/light theme choice in localStorage

--- src/components/darkmode-toggle.tsx
+++ src/components/darkmode-toggle.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useEffect } from 'react';
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+
 
 type Props = {
   className?: string,
@@ -7,9 +11,23 @@ type Props = {
 }
 
 export default function DarkmodeToggle(props: Props) {
+  useEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      props.setDarkmode(true);
+      document.documentElement.classList.add('dark');
+    }
+  });
+
   const toggleTheme = () => {
-    props.setDarkmode(!props.darkmode);
-    document.documentElement.classList.toggle('dark');
+    if (document.documentElement.classList.contains('dark')) {
+      props.setDarkmode(false);
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      props.setDarkmode(true);
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
   }
 
   return (
