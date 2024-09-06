5cc32c349d79e187ce46159491c3d50d173c73f1
David Deprost <dadeprost@gmail.com>
2024-09-06 15:30:23 +0200

    Implement EthNetworkToggle Select component with Headless UI

--- src/components/eth-network-toggle.tsx
+++ src/components/eth-network-toggle.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
+import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/react';
 
 
 type Props = {
@@ -21,12 +22,35 @@ export default function EthNetworkToggle(props: Props) {
   }
 
   return (
-    <Image
+    <Listbox>
+      <ListboxButton>
+        <Image
           src={ resolvedTheme === 'dark' ? '/ethereum-logo-light.svg' : '/ethereum-logo.svg' }
           width={0}
           height={0}
           alt='Ethereum Logo'
           className={props.className}
-      />
+        />
+      </ListboxButton>
+      <ListboxOptions
+        className='bg-[var(--main-bg-color)]
+                   border-2 border-[var(--border-color)]
+                   rounded
+                   z-50'
+        anchor={{ to: 'bottom end',
+                  gap: '.5rem'
+                }}
+      >
+        <ListboxOption value={``} className='m-2'>
+          Ethereum Mainnet
+        </ListboxOption>
+        <ListboxOption value={``} className='m-2'>
+          Testnet Sepolia
+        </ListboxOption>
+        <ListboxOption value={``} className='m-2'>
+          Testnet Holesky
+        </ListboxOption>
+      </ListboxOptions>
+    </Listbox>
   );
 }
