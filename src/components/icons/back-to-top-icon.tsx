b89329b4dbd02e175dcd72b71da9bbf19dd9bee0
David Deprost <dadeprost@gmail.com>
2024-11-12 22:25:16 +0100

    Fix icon errors

--- src/components/icons/back-to-top-icon.tsx
+++ src/components/icons/back-to-top-icon.tsx
@@ -12,9 +12,9 @@ export default function BackToTopIcon(props: Props) {
       viewBox='0 0 48 48'
       fill='none'
     >
-      <path d='M24.0083 14.1005V42' stroke='currentColor' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'/>
-      <path d='M12 26L24 14L36 26' stroke='currentColor' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'/>
-      <path d='M12 6H36' stroke='currentColor' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'/>
+      <path d='M24.0083 14.1005V42' stroke='currentColor' strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'/>
+      <path d='M12 26L24 14L36 26' stroke='currentColor' strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'/>
+      <path d='M12 6H36' stroke='currentColor' strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'/>
     </svg>
   );
 }
