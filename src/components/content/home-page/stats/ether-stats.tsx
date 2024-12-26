964a3ff725e0bac0578540244107c7a3a2e759e1
David Deprost <dadeprost@gmail.com>
2024-12-26 21:10:12 +0100

    Update missing eth eur price bug

--- src/components/content/home-page/stats/ether-stats.tsx
+++ src/components/content/home-page/stats/ether-stats.tsx
@@ -2,7 +2,7 @@ import { Utils } from 'alchemy-sdk';
 
 
 export default async function PriceStats() {
-  let price, supply;
+  let price, ethPrice, supply;
 
   try {
     const response = await fetch(
@@ -11,6 +11,19 @@ export default async function PriceStats() {
     );
     const data = await response.json();
     price = data.ethereum;
+    console.log('price = ', price);
+    ethPrice = {
+      eur: price.eur.toLocaleString('en-US',
+            {
+              style: 'currency',
+              currency: 'EUR',
+            }),
+      usd: price.usd.toLocaleString('en-US',
+            {
+              style: 'currency',
+              currency: 'USD',
+            }),
+    };
   } catch(error) {
     console.error('Coingecko Eth Price Error: ', error);
   }
@@ -26,19 +39,7 @@ export default async function PriceStats() {
     console.error('Etherscan Eth Supply Error: ', error);
   }
 
-  let ethPrice, ethMarketCap;
-  ethPrice = {
-    eur: price.eur.toLocaleString('en-US',
-          {
-            style: 'currency',
-            currency: 'EUR',
-          }),
-    usd: price.usd.toLocaleString('en-US',
-          {
-            style: 'currency',
-            currency: 'USD',
-          }),
-  };
+  let ethMarketCap;
   if (supply) {
     ethMarketCap = {
       eur: (supply * price.eur)
