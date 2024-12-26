d8bb53f2351611bc9dfd50aec00993fd8b1b5da7
David Deprost <dadeprost@gmail.com>
2024-12-26 20:12:27 +0100

    Upgrade to Next 15 to disable serverComponentsHmrCache to fix 'Failed to generate cache key for'

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    /* Fix: "Failed to generate cache key for"
+    // https://github.com/connectrpc/connect-es/issues/1326
+    // https://nextjs.org/docs/app/api-reference/config/next-config-js/serverComponentsHmrCache
+    */
+    serverComponentsHmrCache: false, // defaults to true
+  },
   async redirects() {
     return [
       {
