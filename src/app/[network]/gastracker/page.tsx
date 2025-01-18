6bd0b65756c347084195d6e839e7463e5dc5b3d2
David Deprost <dadeprost@gmail.com>
2025-01-19 00:07:19 +0100

    Merge EtherStats and TransactionStats into updated Stats component

--- src/app/[network]/gastracker/page.tsx
+++ src/app/[network]/gastracker/page.tsx
@@ -1,21 +1,27 @@
-import { Square3Stack3DIcon } from '@heroicons/react/24/outline';
+import NotFoundPage from '@/components/content/error-page/not-found-page';
+import { ChartBarIcon } from '@heroicons/react/24/outline';
 
 
-export default async function TransactionStats() {
-  let totalTransactions, transactionsToday, ethPrice;
-  let lowGasPrice, averageGasPrice, highGasPrice;
+export default async function Page({params} : {params: Promise<{network: string}>})
+{
+  const network = (await params).network;
+  if (network !== 'mainnet') {
+    return <NotFoundPage reason={`There only exists a Gas Tracker for Ethereum Mainnet.`} />;
+  }
 
-  try {
-    const response = await fetch('https://eth.blockscout.com/api/v2/stats');
-    const data = await response.json();
-    totalTransactions = (+data.total_transactions).toLocaleString('en-US');
-    transactionsToday = (+data.transactions_today).toLocaleString('en-US');
-    ethPrice = +data.coin_price;
-    lowGasPrice = +data.gas_prices.slow;
-    averageGasPrice = +data.gas_prices.average;
-    highGasPrice = +data.gas_prices.fast;
-  } catch(error) {
-    console.error('Blockscout Transactions Error: ', error);
+  let ethPrice, lowGasPrice, averageGasPrice, highGasPrice;
+
+  while (!ethPrice) {
+    try {
+      const response = await fetch('https://eth.blockscout.com/api/v2/stats');
+      const data = await response.json();
+      ethPrice = +data.coin_price;
+      lowGasPrice = +data.gas_prices.slow;
+      averageGasPrice = +data.gas_prices.average;
+      highGasPrice = +data.gas_prices.fast;
+    } catch(error) {
+      console.error('Blockscout Gastracker Error: ', error);
+    }
   }
 
   const avgGasAmountPerTransfer = 21000;
@@ -32,35 +38,15 @@ export default async function TransactionStats() {
   const highGasPriceUsd = highGasPrice && gweiPrice ?
     (highGasPrice * avgGasAmountPerTransfer * gweiPrice).toLocaleString('en-US', { maximumFractionDigits: 2 }) : '';
 
-
   return (
-    <div className='border-2 border-[var(--border-color)]
-                    rounded-lg w-full md:w-[50%] max-w-[20rem] md:max-w-[32rem] md:min-w-[26rem] md:ml-12 my-4 md:my-8 md:h-[19.5rem]'>
-      <div className='flex mb-4 border-b-2 border-[var(--border-color)]'>
-        <div>
-          <Square3Stack3DIcon className='w-8 h-8 ml-1.5 mt-1 md:mt-2' />
+    <main>
+      <div className='flex flex-col items-center py-4 pr-16'>
+        <div className='flex py-10'>
+          <ChartBarIcon className='w-8 h-8' />
+          <p className='pt-1 pl-4 text-lg tracking-wider'>ETHEREUM GAS TRACKER</p>
         </div>
-        <div className='ml-4'>
-          <h2 className='font-bold py-2 md:py-3'>
-            Transaction Stats
-          </h2>
-        </div>
-      </div>
-
-      <div className='pl-4 pb-2 border-b border-[var(--border-color)]'>
-        <p className='text-xs tracking-wider text-[var(--grey-fg-color)]'>TOTAL TRANSACTIONS</p>
-        <p>{ totalTransactions }</p>
-      </div>
-
-      <div className='pl-4 pt-4 pb-2 border-b border-[var(--border-color)]'>
-        <p className='text-xs tracking-wider text-[var(--grey-fg-color)]'>TRANSACTIONS TODAY</p>
-        <p>{ transactionsToday }</p>
-      </div>
-
-      <div className='px-4 pt-4 pb-2'>
-        <p className='text-xs tracking-wider text-[var(--grey-fg-color)]'>GAS TRACKER</p>
-        <div className='flex flex-col md:flex-row items-start justify-between w-full mt-2'>
-          <div>
+        <div className='flex flex-col md:flex-row items-start justify-between w-full max-w-[50rem] mt-2'>
+          <div className='border border-[var(--border-color)] rounded-lg md:px-16 md:py-4'>
             <p className='font-bold'>
               <span className='text-xl mr-2' role="img" aria-label="smiling face with sunglasses">😎</span>
               Low
@@ -68,7 +54,7 @@ export default async function TransactionStats() {
             <p className='text-lg tracking-wide text-green-600'>{ lowGasPriceGwei } gwei</p>
             <p className='text-sm tracking-wide text-green-600'>(${ lowGasPriceUsd })</p>
           </div>
-          <div className='my-4 md:my-0'>
+          <div className='border border-[var(--border-color)] rounded-lg my-4 md:my-0 md:px-16 md:py-4'>
             <p className='font-bold'>
               <span className='text-xl mr-2' role="img" aria-label="beaming face with smiling eyes">😁</span>
               Average
@@ -76,7 +62,7 @@ export default async function TransactionStats() {
             <p className='text-lg tracking-wide text-blue-600'>{ averageGasPriceGwei } gwei</p>
             <p className='text-sm tracking-wide text-blue-600'>(${ averageGasPriceUsd })</p>
           </div>
-          <div>
+          <div className='border border-[var(--border-color)] rounded-lg md:px-16 md:py-4'>
             <p className='font-bold'>
               <span className='text-xl mr-2' role="img" aria-label="flushed face">😳</span>
               High
@@ -86,6 +72,6 @@ export default async function TransactionStats() {
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
--- src/components/content/home-page/stats/index.tsx
+++ src/components/content/home-page/stats/index.tsx
@@ -1,12 +1,140 @@
-import EtherStats from './ether-stats';
-import TransactionStats from './transaction-stats';
+import { Utils } from 'alchemy-sdk';
+import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { Square3Stack3DIcon } from '@heroicons/react/24/outline';
+import { FireIcon } from '@heroicons/react/24/outline';
+import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 
 export default async function Stats() {
+  let EthSupply, Eth2Staking, BurntFees;
+  let ethSupplyError = false;
+
+  while (!EthSupply && !ethSupplyError) {
+    try {
+      const response = await fetch(`https://api.etherscan.io/api?module=stats&action=ethsupply2&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`);
+      const data = await response.json();
+      ({ EthSupply, Eth2Staking, BurntFees } = data.result);
+    } catch(error) {
+      console.error('Etherscan Eth Supply Error: ', error);
+      if (error instanceof SyntaxError) { // SyntaxError in json parsing
+        ethSupplyError = true;
+      }
+    }
+  }
+
+  let totalTransactions, transactionsToday, ethPrice, averageGasPrice;
+
+  while (!ethPrice) {
+    try {
+      const response = await fetch('https://eth.blockscout.com/api/v2/stats');
+      const data = await response.json();
+      totalTransactions = (+data.total_transactions).toLocaleString('en-US');
+      transactionsToday = (+data.transactions_today).toLocaleString('en-US');
+      ethPrice = +data.coin_price;
+      averageGasPrice = +data.gas_prices.average;
+    } catch(error) {
+      console.error('Blockscout Transactions Stats Error: ', error);
+    }
+  }
+
+  const avgGasAmountPerTransfer = 21000;
+  const gweiPrice = ethPrice ? (ethPrice / 1e9) : undefined;
+
+  const averageGasPriceGwei = averageGasPrice ? averageGasPrice.toLocaleString('en-US', { maximumFractionDigits: 3 }) : '';
+  const averageGasPriceUsd = averageGasPrice && gweiPrice ?
+    (averageGasPrice * avgGasAmountPerTransfer * gweiPrice).toLocaleString('en-US', { maximumFractionDigits: 2 }) : '';
+
+  let supply = ethSupplyError ? null : +Utils.formatEther(EthSupply)
+              + (+Utils.formatEther(Eth2Staking))
+              - (+Utils.formatEther(BurntFees));
+
+  let ethMarketCap, supplyFormatted, ethPriceFormatted;
+  if (supply && ethPrice) {
+    ethMarketCap = (supply * ethPrice)
+            .toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            });
+    supplyFormatted = supply.toLocaleString('en-US', {
+      maximumFractionDigits: 2,
+    });
+    ethPriceFormatted = ethPrice.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+  }
+
   return (
-    <div className='flex flex-col md:flex-row items-center md:items-start w-full'>
-      <EtherStats />
-      <TransactionStats />
+    <div className='border-2 border-[var(--border-color)]
+                    rounded-lg w-full max-w-[80rem] my-4 md:my-8'>
+      <div className='flex flex-col md:flex-row justify-between'>
+        <div className='w-56 pl-4 py-4'>
+          <div className='flex'>
+            <CurrencyDollarIcon className='w-8 h-8' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER PRICE</p>
+          </div>
+          <div className='pl-12'>
+            {ethPriceFormatted}
+          </div>
+        </div>
+
+        <div className='w-56 pl-4 py-0 md:py-4'>
+          <div className='flex'>
+            <div className='w-8 h-8 bg-[image:var(--eth-logo-url)] bg-contain bg-no-repeat bg-center' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER SUPPLY</p>
+          </div>
+          <div className='pl-12'>
+            {`Ξ${supplyFormatted}`}
+          </div>
+        </div>
+
+        <div className='w-60 px-4 py-4'>
+          <div className='flex'>
+            <GlobeAltIcon className='w-8 h-8' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER MARKET CAP</p>
+          </div>
+          <div className='pl-12'>
+            {ethMarketCap}
+          </div>
+        </div>
+      </div>
+
+      <div className='flex flex-col md:flex-row justify-between'>
+        <div className='w-56 pl-4 py-0 md:py-4'>
+          <div className='flex'>
+          <FireIcon className='w-8 h-8' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>AVERAGE GAS PRICE</p>
+          </div>
+          <Link
+            href='/mainnet/gastracker'
+            className='pl-12 hover:text-[var(--hover-fg-color)] dark:hover:text-[var(--inverse-bg-color-lighter)]'
+          >
+            {averageGasPriceGwei} gwei (${averageGasPriceUsd})
+          </Link>
+        </div>
+
+        <div className='w-56 pl-4 pt-4 md:py-4'>
+          <div className='flex'>
+          <ClipboardDocumentListIcon className='w-8 h-8' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>TRANSACTIONS TODAY</p>
+          </div>
+          <div className='pl-12'>
+            {transactionsToday}
+          </div>
+        </div>
+
+        <div className='w-60 px-4 py-4'>
+          <div className='flex'>
+            <Square3Stack3DIcon className='w-8 h-8' />
+            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>TOTAL TRANSACTIONS</p>
+          </div>
+          <div className='pl-12'>
+            {totalTransactions}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
