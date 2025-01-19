94c89bd1630c5b4ed422a860bc72143bb9057df6
David Deprost <dadeprost@gmail.com>
2025-01-19 12:33:26 +0100

    Put borders in Stats component

--- src/components/content/home-page/stats/index.tsx
+++ src/components/content/home-page/stats/index.tsx
@@ -74,7 +74,7 @@ export default async function Stats() {
     <div className='border-2 border-[var(--border-color)]
                     rounded-lg w-full max-w-[75rem] my-4 md:my-8'>
       <div className='flex flex-col md:flex-row justify-between'>
-        <div className='w-56 pl-4 py-4'>
+        <div className='w-56 md:w-[calc(100%/3)] pl-4 py-4 md:border-b border-[var(--border-color)]'>
           <div className='flex'>
             <CurrencyDollarIcon className='w-8 h-8' />
             <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER PRICE</p>
@@ -84,7 +84,7 @@ export default async function Stats() {
           </div>
         </div>
 
-        <div className='w-56 pl-4 py-0 md:py-4'>
+        <div className='w-56 md:w-[calc(100%/3)] pl-4 md:pl-[calc(100%/18)] py-0 md:py-4 md:border-b md:border-x border-[var(--border-color)]'>
           <div className='flex'>
             <div className='w-8 h-8 bg-[image:var(--eth-logo-url)] bg-contain bg-no-repeat bg-center' />
             <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER SUPPLY</p>
@@ -94,19 +94,21 @@ export default async function Stats() {
           </div>
         </div>
 
-        <div className='w-60 px-4 py-4'>
-          <div className='flex'>
-            <GlobeAltIcon className='w-8 h-8' />
-            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER MARKET CAP</p>
-          </div>
-          <div className='pl-12'>
-            {ethMarketCap}
+        <div className='w-60 md:w-[calc(100%/3)] px-4 py-4 md:border-b border-[var(--border-color)]'>
+          <div className='block w-60 ml-auto mr-auto'>
+            <div className='flex'>
+              <GlobeAltIcon className='w-8 h-8' />
+              <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>ETHER MARKET CAP</p>
+            </div>
+            <div className='pl-12'>
+              {ethMarketCap}
+            </div>
           </div>
         </div>
       </div>
 
       <div className='flex flex-col md:flex-row justify-between'>
-        <div className='w-56 pl-4 py-0 md:py-4'>
+        <div className='w-56 md:w-[calc(100%/3)] pl-4 py-0 md:py-4'>
           <div className='flex'>
           <FireIcon className='w-8 h-8' />
             <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>AVERAGE GAS PRICE</p>
@@ -119,7 +121,7 @@ export default async function Stats() {
           </Link>
         </div>
 
-        <div className='w-56 pl-4 pt-4 md:py-4'>
+        <div className='w-56 md:w-[calc(100%/3)] pl-4 md:pl-[calc(100%/18)] pt-4 md:py-4 md:border-x border-[var(--border-color)]'>
           <div className='flex'>
           <ClipboardDocumentListIcon className='w-8 h-8' />
             <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>TRANSACTIONS TODAY</p>
@@ -129,13 +131,15 @@ export default async function Stats() {
           </div>
         </div>
 
-        <div className='w-60 px-4 py-4'>
-          <div className='flex'>
-            <Square3Stack3DIcon className='w-8 h-8' />
-            <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>TOTAL TRANSACTIONS</p>
-          </div>
-          <div className='pl-12'>
-            {totalTransactions}
+        <div className='w-60 md:w-[calc(100%/3)] px-4 py-4'>
+          <div className='block w-60 ml-auto mr-auto'>
+            <div className='flex'>
+              <Square3Stack3DIcon className='w-8 h-8' />
+              <p className='pt-2 pl-4 text-xs tracking-wider text-[var(--grey-fg-color)]'>TOTAL TRANSACTIONS</p>
+            </div>
+            <div className='pl-12'>
+              {totalTransactions}
+            </div>
           </div>
         </div>
       </div>
