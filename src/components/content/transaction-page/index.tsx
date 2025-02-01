d4cdc932366cc5197393495c8bb7e10dd88124d4
David Deprost <dadeprost@gmail.com>
2025-02-01 01:57:27 +0100

    Fix occasional transaction page error, probably due to loop failing

--- src/components/content/transaction-page/index.tsx
+++ src/components/content/transaction-page/index.tsx
@@ -11,19 +11,16 @@ type Props = {
 export default async function TransactionPage(props: Props) {
   let tx, txReceipt;
 
-  while (!tx) {
-    try {
-      tx = await props.alchemy.transact.getTransaction(props.hash);
-    } catch (err) {
-      console.error('getTransaction()', err);
-    }
+  try {
+    tx = await props.alchemy.transact.getTransaction(props.hash);
+  } catch (err) {
+    console.error('getTransaction()', err);
   }
-  while (!txReceipt) {
-    try {
-      txReceipt = await props.alchemy.core.getTransactionReceipt(props.hash);
-    } catch (err) {
-      console.error('getTransactionReceipt()', err);
-    }
+
+  try {
+    txReceipt = await props.alchemy.core.getTransactionReceipt(props.hash);
+  } catch (err) {
+    console.error('getTransactionReceipt()', err);
   }
 
   return (
@@ -42,13 +39,16 @@ export default async function TransactionPage(props: Props) {
           <p className='md:flex'>
             <span className='w-60 md:pl-[5.25rem]'>Status:</span>
             {
-              txReceipt.status ?
-                <span className='bg-green-100 text-green-700 border-green-400 border rounded-md p-1 px-4 w-[6.4rem] ml-4 md:ml-0'>
-                  Success
-                </span> :
-                <span className='bg-red-100 text-red-700 border-red-400 border rounded-md p-1 px-4 w-[5rem] ml-4 md:ml-0'>
-                  Fail
-                </span>
+              txReceipt ?
+                txReceipt.status ?
+                  <span className='bg-green-100 text-green-700 border-green-400 border rounded-md p-1 px-4 w-[6.4rem] ml-4 md:ml-0'>
+                    Success
+                  </span> :
+                  <span className='bg-red-100 text-red-700 border-red-400 border rounded-md p-1 px-4 w-[5rem] ml-4 md:ml-0'>
+                    Fail
+                  </span>
+                :
+                <span className='text-red-500 ml-4 md:ml-0'>Error getting data.</span>
             }
           </p>
         </div>
@@ -56,65 +56,109 @@ export default async function TransactionPage(props: Props) {
           <p className='md:flex'>
             <span className='w-60'>Block:</span>
             <span>
-              <Link
-                href={`/${props.network}/block/${tx.blockNumber}`}
-                className='ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
-              >
-                {tx.blockNumber}
-              </Link>
+              {
+              tx ?
+                <Link
+                  href={`/${props.network}/block/${tx.blockNumber}`}
+                  className='ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
+                >
+                  {tx.blockNumber}
+                </Link>
+                :
+                <span className='text-red-500 ml-2 md:ml-0'>Error getting data.</span>
+              }
             </span>
           </p>
         </li>
         <div className='ml-4'>
           <p className='md:flex'>
             <span className='w-60 md:pl-[1.55rem]'>Confirmations:</span>
-            <span className='ml-2 md:ml-0'>{tx.confirmations}</span>
+            <span className='ml-2 md:ml-0'>
+              {
+                tx ? tx.confirmations : <span className='text-red-500'>Error getting data.</span>
+              }
+            </span>
           </p>
         </div>
         <li className='list-disc ml-4 mt-4 m-2'>
           <p className='md:flex'>
             <span className='w-60'>Value:</span>
-            <span className='ml-2 md:ml-0'>Ξ{Utils.formatEther(tx.value)}</span>
+            <span className='ml-2 md:ml-0'>
+              {
+                tx ? `Ξ${Utils.formatEther(tx.value)}` : <span className='text-red-500'>Error getting data.</span>
+              }
+            </span>
           </p>
         </li>
         <div className='ml-4'>
           <p className='md:flex'>
             <span className='w-60 pl-2 md:pl-[5.8rem]'>From:</span>
-            <Link
-              href={`/${props.network}/address/${tx.from}`}
-              className='font-mono ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
-            >
-              {tx.from}
-            </Link>
+            {
+              tx ?
+                <Link
+                  href={`/${props.network}/address/${tx.from}`}
+                  className='font-mono ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
+                >
+                  {tx.from}
+                </Link>
+                :
+                <span className='text-red-500 ml-2 md:ml-0'>Error getting data.</span>
+            }
           </p>
         </div>
         <div className='ml-4 pt-2 md:pt-0'>
           <p className='md:flex'>
             <span className='w-60 pl-2 md:pl-[7.05rem]'>To:</span>
-            <Link
-              href={`/${props.network}/address/${tx.to}`}
-              className='font-mono ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
-            >
-              {tx.to}
-            </Link>
+            {
+              tx ?
+                <Link
+                  href={`/${props.network}/address/${tx.to}`}
+                  className='font-mono ml-2 md:ml-0 text-[var(--link-color)] hover:text-[var(--hover-fg-color)]'
+                >
+                  {tx.to}
+                </Link>
+                :
+                <span className='text-red-500 ml-2 md:ml-0'>Error getting data.</span>
+            }
           </p>
         </div>
         <li className='list-disc ml-4 mt-4 m-2'>
           <p className='md:flex'>
             <span className='w-60 text-nowrap'>Transaction Fee:</span>
-            <span className='ml-2 md:ml-0'>Ξ{Utils.formatEther(tx.gasPrice!.mul(txReceipt.gasUsed))} (= Gas Price * Gas Used)</span>
+            <span className='ml-2 md:ml-0'>
+              {
+                tx && txReceipt ?
+                  `Ξ${Utils.formatEther(tx.gasPrice!.mul(txReceipt.gasUsed))} (= Gas Price * Gas Used)`
+                  :
+                  <span className='text-red-500'>Error getting data.</span>
+              }
+            </span>
           </p>
         </li>
         <div className='ml-4 pt-2 md:pt-0'>
           <p className='md:flex'>
             <span className='w-60 pl-2 md:pl-[3.7rem]'>Gas Price:</span>
-            <span className='ml-2 md:ml-0'>{+Utils.formatEther(tx.gasPrice!) * Math.pow(10, 9)} Gwei (Ξ{Utils.formatEther(tx.gasPrice!)})</span>
+            <span className='ml-2 md:ml-0'>
+              {
+                tx ?
+                  `${+Utils.formatEther(tx.gasPrice!) * Math.pow(10, 9)} Gwei (Ξ${Utils.formatEther(tx.gasPrice!)})`
+                  :
+                  <span className='text-red-500'>Error getting data.</span>
+              }
+              </span>
           </p>
         </div>
         <div className='ml-4 pt-2 md:pt-0'>
           <p className='md:flex'>
             <span className='w-60 pl-2 md:pl-[3.65rem]'>Gas Used:</span>
-            <span className='ml-2 md:ml-0'>{+Utils.formatEther(txReceipt.gasUsed) * Math.pow(10, 9)} Gwei (Ξ{Utils.formatEther(txReceipt.gasUsed)})</span>
+            <span className='ml-2 md:ml-0'>
+              {
+                txReceipt ?
+                  `${+Utils.formatEther(txReceipt.gasUsed) * Math.pow(10, 9)} Gwei (Ξ${Utils.formatEther(txReceipt.gasUsed)})`
+                  :
+                  <span className='text-red-500'>Error getting data.</span>
+              }
+              </span>
           </p>
         </div>
       </ul>
