442941f6a1767fb330f8ca62f8192583e50c5b6c
David Deprost <dadeprost@gmail.com>
2025-02-01 01:58:02 +0100

    Turn serverComponentsHmrCache back on

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
+  // experimental: {
     /* Fix: "Failed to generate cache key for"
     // https://github.com/connectrpc/connect-es/issues/1326
     // https://nextjs.org/docs/app/api-reference/config/next-config-js/serverComponentsHmrCache
     */
-    serverComponentsHmrCache: false, // defaults to true
-  },
+    // serverComponentsHmrCache: false, // defaults to true
+  // },
   async redirects() {
     return [
       {
