1ad06b74e00ad37ea0a33b3ec7db443d689e2dac
David Deprost <dadeprost@gmail.com>
2025-02-02 21:28:01 +0100

    Revert basePath and add semicolons to build command

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   output: 'standalone',
   // Map all static assets to the project URL davidde.github.io/ethblox,
   // instead of the base davidde.github.io domain, but only for production:
-  // basePath: isProd ? '/ethblox' : undefined,
+  basePath: isProd ? '/ethblox' : undefined,
   // Note that all `npm run build` commands will get classified as 'production',
   // so they will get the '/ethblox' basePath even when run locally.
   // This means that when running the build with `node .next/standalone/server.js` locally,
