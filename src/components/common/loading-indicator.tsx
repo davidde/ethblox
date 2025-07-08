931e3cc31d8af69b59cfcd8639c064b3f251fa2e
David Deprost <dadeprost@gmail.com>
2025-07-08 20:29:15 +0200

    Add animation to LoadingIndicator

--- src/components/common/loading-indicator.tsx
+++ src/components/common/loading-indicator.tsx
@@ -1,7 +1,19 @@
 export default function LoadingIndicator() {
+  const letters = ['L', 'o', 'a', 'd', 'i', 'n', 'g', '\u00A0', '.', '.', '.'];
+
   return (
-    <span className='italic text-neutral-500'>
-      Loading ...
-    </span>
+    <ul className='flex list-none p-0 m-0'>
+      {
+        letters.map((char, index) =>
+          <li
+            key={index}
+            className='italic animate-(--animate-glow)'
+            style={{animationDelay: `${index * 0.1}s`}}
+          >
+            {char}
+          </li>
+        )
+      }
+    </ul>
   );
 }
