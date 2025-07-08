0b802344febc6ad9e09dece31984f460c7748930
David Deprost <dadeprost@gmail.com>
2025-07-08 21:18:15 +0200

    Fix LoadingIndicator layout shift
    Hard to find the cause:
    The layout shift is caused because the <ul> is a block element by default.
    Apparently this causes the ul to render below the span initially,
    before the CSS is loaded, and only afterwards correcting for flex-row,
    and moving sideways of the span. CSS fixes did not work at all.
    The only possible solution is to use an inline-default element like <span>!

--- src/components/common/loading-indicator.tsx
+++ src/components/common/loading-indicator.tsx
@@ -2,18 +2,18 @@ export default function LoadingIndicator() {
   const letters = ['L', 'o', 'a', 'd', 'i', 'n', 'g', '\u00A0', '.', '.', '.'];
 
   return (
-    <ul className='flex list-none p-0 m-0'>
+    <span className='inline-flex p-0 m-0'>
       {
         letters.map((char, index) =>
-          <li
+          <span
             key={index}
             className='italic animate-(--animate-glow)'
             style={{animationDelay: `${index * 0.1}s`}}
           >
             {char}
-          </li>
+          </span>
         )
       }
-    </ul>
+    </span>
   );
 }
