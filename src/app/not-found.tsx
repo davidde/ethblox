5bc43ee41b3a33d66b0108a4c9f9d7a71cbcf73d
David Deprost <dadeprost@gmail.com>
2025-07-09 13:29:35 +0200

    Unify both 404 pages by reusing NotFoundPage is nextjs default not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,8 @@
-import Link from 'next/link';
+import NotFoundPage from '@/components/content/error-page/not-found-page';
 
 
 export default function NotFound() {
   return (
-    <main className='p-2 md:p-8 text-red-700'>
-      <div className='py-8 px-4 mx-auto max-w-(--breakpoint-xl) lg:py-16 lg:px-6'>
-        <div className='mx-auto max-w-(--breakpoint-sm) text-center'>
-          <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500'>404</h1>
-          <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>Not Found.</p>
-          <p className='text-lg font-light text-gray-500 dark:text-gray-400'>Sorry, we can&apos;t find that page.</p>
-          <Link href='/' className='inline-flex text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-hidden focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4'>Back to Homepage</Link>
-        </div>
-      </div>
-    </main>
+    <NotFoundPage />
   );
 }
--- src/components/content/error-page/not-found-page.tsx
+++ src/components/content/error-page/not-found-page.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
 
-type Props = {
-  reason: string
-}
 
-export default function NotFoundPage(props: Props) {
+export default function NotFoundPage(props: { reason?: string }) {
   return (
     <main className='p-2 md:p-8 text-red-700'>
       <div className='py-8 px-4 mx-auto max-w-(--breakpoint-xl) lg:py-16 lg:px-6'>
@@ -12,7 +9,12 @@ export default function NotFoundPage(props: Props) {
           <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500'>404</h1>
           <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>Not Found.</p>
           <p className='text-lg font-light text-gray-500 dark:text-gray-400'>Sorry, we can&apos;t find that page.</p>
-          <p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>{props.reason}</p>
+          {
+            props.reason ?
+              <p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>{props.reason}</p>
+              :
+              ''
+          }
           <Link href='/' className='inline-flex text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-hidden focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4'>Back to Homepage</Link>
         </div>
       </div>
