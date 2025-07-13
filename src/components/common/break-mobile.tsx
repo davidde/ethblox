be8241ddac660501b919fea45d0b3d5c72b59e54
David Deprost <dadeprost@gmail.com>
2025-07-13 20:31:45 +0200

    Update AddressPage

---
+++ src/components/common/break-mobile.tsx
@@ -0,0 +1,3 @@
+export default function BreakMobile() {
+  return <br className='md:hidden' />;
+}
--- src/components/content/address-page/index.tsx
+++ src/components/content/address-page/index.tsx
@@ -6,6 +6,7 @@ import EthBalance from './eth-balance';
 import { useSearchParams } from 'next/navigation';
 import { isAddress } from 'ethers';
 import NotFoundPage from '../error-page/not-found-page';
+import BreakMobile from '@/components/common/break-mobile';
 
 
 export default function AddressPage(props: {network: string}) {
@@ -13,48 +14,37 @@ export default function AddressPage(props: {network: string}) {
   const hash = searchParams.get('hash') ?? '';
   const hashOK = isAddress(hash);
 
+  if (hash.length !== 42)
+    return <NotFoundPage reason={`Surely you're joking, right?\n
+      "${hash}" is not an Ethereum address.`} />;
+  else if (!hashOK) return <div>This is not a valid Ethereum address.</div>
+
+  // Testnet shows no tokens, so Transactions should show instead, and not to the right:
+  const wrapTransactions = props.network === 'mainnet' ? 'md:flex-row flex-wrap' : '';
+
   return (
     <main className='m-4 mt-8 md:m-8'>
-      <div>
-        <h1 className='text-lg font-bold mb-8'>
-          Address Details
-        </h1>
-        <h2 className='capsTitle'>
-          ADDRESS
-        </h2>
-        <p className='max-w-[90vw] break-words'>
-          {hash}
-        </p>
+      <div className='mb-8'>
+        <span className='text-[1.25rem] font-bold'>Address: &nbsp;</span>
+        <BreakMobile />
+        <span className='break-all font-medium'>{hash}</span>
       </div>
-      {
-        hash.length !== 42 ?
-          <NotFoundPage reason={`Surely you're joking, right?\n"${hash}" is not an Ethereum address.`} />
-          :
-          !hashOK ?
-            <div>This is not a valid Ethereum address.</div>
-            :
-            <div className={props.network === 'mainnet' ?
-              'flex flex-col md:flex-row flex-wrap'
-              :
-              'flex flex-col' // Testnet shows no tokens,
-              // so Transactions should show instead (and not to the right)
-            }>
-              <div>
-                <EthBalance
-                  hash={hash}
-                  network={props.network}
-                />
-                <Tokens
-                  hash={hash}
-                  network={props.network}
-                />
-              </div>
-              <Transactions
-                hash={hash}
-                network={props.network}
-              />
-            </div>
-      }
+      <div className={`flex flex-col ${wrapTransactions}`}>
+        <div>
+          <EthBalance
+            hash={hash}
+            network={props.network}
+          />
+          <Tokens
+            hash={hash}
+            network={props.network}
+          />
+        </div>
+        <Transactions
+          hash={hash}
+          network={props.network}
+        />
+      </div>
     </main>
   );
 }
