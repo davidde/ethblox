1916fa39859f180f53c9048ad53ba9fa3e14b30c
David Deprost <dadeprost@gmail.com>
2025-07-14 08:59:56 +0200

    Fix Tokens filter for zero balances

--- src/components/content/address-page/tokens.tsx
+++ src/components/content/address-page/tokens.tsx
@@ -10,52 +10,48 @@ export default function Tokens(props: {
   hash: string,
   network: string
 }) {
+  if (props.network !== 'mainnet') return '';
+
   const alchemy = getAlchemy(props.network);
-  const [realTokens, setRealTokens] = useState<OwnedToken[]>();
-  const [realTokensError, setRealTokensError] = useState<string>();
-  const showTokens = props.network === 'mainnet' ? '' : 'hidden';
+  const [tokens, setTokens] = useState<OwnedToken[]>();
+  const [tokensError, setTokensError] = useState<string>();
 
   useEffect(() => {
     (async () => {
-      if (props.network === 'mainnet') {
-        try {
-          let tokens = await alchemy.core.getTokensForOwner(props.hash);
-          // Remove scam tokens; everything with zero balance or undefined logo or symbol:
-          let filteredTokens = tokens.tokens.filter(token => token.balance !== '0.0' &&
-            token.logo !== undefined && token.symbol !== undefined);
-          // Sort tokens alphabetically by symbol:
-          filteredTokens.sort((a, b) => a.symbol!.localeCompare(b.symbol!));
-          setRealTokens(filteredTokens);
-        } catch(err) {
-          const error = 'AddressPage Tokens getTokensForOwner()' + err;
-          console.error(error);
-          setRealTokensError(error);
-        }
+      try {
+        const resp = await alchemy.core.getTokensForOwner(props.hash);
+        // Remove scam tokens; everything without logo/symbol or zero balance:
+        let filteredTokens = resp.tokens.filter(token =>
+          token.logo && token.symbol && token.balance &&
+          !(token.balance.startsWith('0') && token.balance.endsWith('0')));
+        // Sort tokens alphabetically by symbol:
+        filteredTokens.sort((a, b) => a.symbol!.localeCompare(b.symbol!));
+        setTokens(filteredTokens);
+      } catch(err) {
+        const error = 'AddressPage Tokens getTokensForOwner() ' + err;
+        console.error(error);
+        setTokensError(error);
       }
     })();
-  }, [alchemy, props.hash, props.network]);
+  }, [alchemy, props.hash]);
 
   function getTokenList(tokens: OwnedToken[] | undefined ) {
     if (tokens && tokens.length === 0) return '/';
-    return tokens?.map(
-      (token, i) => {
-        let balance = parseFloat(token.balance ?? '0').toFixed(8);
-        balance = balance.includes('.') && balance.endsWith('0') ?
-          parseFloat(balance).toString() : balance;
-        return <li key={i} className='ml-4 list-disc text-(--grey-fg-color)'>
-          <span className='text-(--main-fg-color)'>
-            {`${token.symbol}: ${balance} (${token.name})`}
-          </span>
-        </li>;
-      });
+    return tokens?.map((token, i) =>
+      <li key={i} className='ml-4 list-disc text-(--grey-fg-color)'>
+        <span className='text-(--main-fg-color)'>
+          {`${token.symbol}: ${parseFloat(token.balance!).toFixed(5)} (${token.name})`}
+        </span>
+      </li>
+    );
   }
 
   return (
-    <div className={`${showTokens} pr-12 my-4`}>
+    <div className='pr-12 my-4'>
       <h2 className='capsTitle'>TOKEN HOLDINGS</h2>
       <ValueDisplay
-        value={getTokenList(realTokens)}
-        error={realTokensError}
+        value={getTokenList(tokens)}
+        error={tokensError}
         err='Error getting tokens'
       />
     </div>
