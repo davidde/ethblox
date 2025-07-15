642f433571b1e0d85e72dc2fe8ab9ec6c16b9f14
David Deprost <dadeprost@gmail.com>
2025-07-15 09:19:37 +0200

    Move AddressPage Transactions ROW function into separate TxRow component

--- src/components/content/address-page/transactions/transactions-view.tsx
+++ src/components/content/address-page/transactions/transactions-view.tsx
@@ -1,6 +1,7 @@
-import PopoverLink from "@/components/common/popover-link";
-import { truncateAddress, truncateTransaction } from "@/lib/utilities";
-import Link from "next/link";
+import PopoverLink from '@/components/common/popover-link';
+import { truncateAddress, truncateTransaction } from '@/lib/utilities';
+import Link from 'next/link';
+import TxRow from './tx-row';
 
 
 export default function TransactionsView(props: {
@@ -16,14 +17,14 @@ export default function TransactionsView(props: {
         href={`/${props.network}/transaction?hash=${tx.hash}`}
         content={truncateTransaction(tx.hash, 18)!}
         popover={tx.hash}
-        className="-left-full top-[-2.6rem] w-120 py-1.5 px-2.5"
+        className='-left-full top-[-2.6rem] w-120 py-1.5 px-2.5'
       />
   )}, {
     name: 'Block',
     render: (tx: any) => (
       <Link
         href={`/${props.network}/block?number=${tx.block}`}
-        className="text-(--link-color) hover:text-(--hover-fg-color)"
+        className='text-(--link-color) hover:text-(--hover-fg-color)'
       >
         {tx.block}
       </Link>
@@ -37,7 +38,7 @@ export default function TransactionsView(props: {
         href={`/${props.network}/address?hash=${tx.from}`}
         content={truncateAddress(tx.from, 21)!}
         popover={tx.from}
-        className="left-[-35%] top-[-2.6rem] w-78 py-1.5 px-2.5"
+        className='left-[-35%] top-[-2.6rem] w-78 py-1.5 px-2.5'
       />
   )}, {
     name: 'To',
@@ -46,31 +47,13 @@ export default function TransactionsView(props: {
                 href={`/${props.network}/address?hash=${tx.to}`}
                 content={truncateAddress(tx.to, 21)}
                 popover={tx.to}
-                className="left-[-35%] top-[-2.6rem] w-78 py-1.5 px-2.5"
+                className='left-[-35%] top-[-2.6rem] w-78 py-1.5 px-2.5'
               />
               : <span>/</span>,
   }, {
     name: 'Amount',
     render: (tx: any) => <span>{tx.amount}</span>,
   }];
-
-  const ROW = (tx: any, isDesktop: boolean, index: number) => isDesktop ?
-    <tr key={index} className='w-full border-b border-(--border-color) last-of-type:border-none py-3'>
-      { COLUMNS.map((col, i) => (
-        <td key={i} className='whitespace-nowrap px-4 py-3'>{ col.render(tx) }</td>
-      )) }
-    </tr>
-    :
-    <div key={index} className='mb-2 w-full py-2 border-b border-(--border-color) last-of-type:border-none'>
-      { COLUMNS.map((col, i) => (
-        <div key={i} className='pb-1'>
-          <span className={`font-medium ${col.name === 'To' ? 'pl-5' : ''}`}>
-            { col.name }: &nbsp;
-          </span>
-            { col.render(tx) }
-        </div>
-      )) }
-    </div>
 
   return (
     <div>
@@ -87,18 +70,19 @@ export default function TransactionsView(props: {
               ))}
             </tr>
           </thead>
-          <tbody>
-            { // Content rows of table on Desktop (isDesktop = true):
-              props.transactions.map((tx, i) => ROW(tx, true, i)) }
-          </tbody>
+          <tbody>{ // Content rows of table on Desktop (isMedium = true):
+            props.transactions.map((tx, i) =>
+              <TxRow key={i} tx={tx} cols={COLUMNS} isMedium={true} />)
+          }</tbody>
         </table>
       </div>
 
       {/* Mobile display only: */}
       <div className='block md:hidden'>
-        { // Content rows on mobile (isDesktop = false):
-          props.transactions.map((tx, i) => ROW(tx, false, i)) }
-      </div>
+      { // Content rows on mobile (isMedium = false):
+        props.transactions.map((tx, i) =>
+          <TxRow key={i} tx={tx} cols={COLUMNS} isMedium={false} />)
+      }</div>
     </div>
   );
 }
---
+++ src/components/content/address-page/transactions/tx-row.tsx
@@ -0,0 +1,32 @@
+export default function TxRow(props: {
+  tx: any,
+  cols: any[],
+  isMedium: boolean, // True for desktop, false for mobile
+}) {
+  if (props.isMedium)
+    return ( // Desktop View:
+      <tr className='w-full border-b border-(--border-color) last-of-type:border-none py-3'>
+        {
+          props.cols.map((col, i) => (
+            <td key={i} className='whitespace-nowrap px-4 py-3'>
+              { col.render(props.tx) }
+            </td>
+          ))
+        }
+      </tr>
+    );
+  else return ( // Mobile View:
+    <div className='mb-2 w-full py-2 border-b border-(--border-color) last-of-type:border-none'>
+      {
+        props.cols.map((col, i) => (
+          <div key={i} className='pb-1'>
+            <span className={`font-medium ${col.name === 'To' ? 'pl-5' : ''}`}>
+              { col.name }: &nbsp;
+            </span>
+            { col.render(props.tx) }
+          </div>
+        ))
+      }
+    </div>
+  );
+}
