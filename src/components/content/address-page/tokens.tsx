ba5180dc5612c25ad55d42eb802b1bd8ff3f5eb2
David Deprost <dadeprost@gmail.com>
2025-07-16 21:59:38 +0200

    Add Transition to Tokens Dropdown Menu

--- src/components/content/address-page/tokens.tsx
+++ src/components/content/address-page/tokens.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import ValueDisplay from '@/components/common/value-display';
 import { getAlchemy } from '@/lib/utilities';
 import { OwnedToken } from 'alchemy-sdk';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/16/solid';
 
 
@@ -58,24 +58,32 @@ export default function Tokens(props: {
 
     return (
       <Menu as='div' className='relative inline-block text-left'>
-        <MenuButton className='relative w-[15rem] inline-flex items-center justify-between rounded-md
+        <MenuButton className='relative z-20 w-[15rem] inline-flex items-center justify-between rounded-md
             bg-(--card-bg-color) text-(--main-fg-color) px-3 py-1.5 text-sm/6 font-medium
             border border-(--border-color) shadow-inner shadow-black/15 dark:shadow-white/10
-          hover:bg-(--banner-bg-color) data-open:bg-(--banner-bg-color)'>
+            hover:bg-(--banner-bg-color) data-open:bg-(--banner-bg-color) cursor-pointer'>
           {tokens.length} Tokens
           <ChevronDownIcon className='size-4' />
         </MenuButton>
-
-        <MenuItems className='absolute left-0 origin-top-left z-10 overflow-auto
-          mt-2 max-h-[23rem] min-w-[15rem] max-w-[25rem] rounded-md shadow-black/40 dark:shadow-white/5
-          shadow-2xl ring-1 ring-(--border-color) bg-(--comp-bg-color) text-(--main-fg-color)
-          flex flex-col divide-y divide-(--border-color)/60'>
-            {tokens.map((token) => (
-              <MenuItem key={token.address}>
+        <Transition
+          as={Fragment} // Using Fragment ensures no extra DOM node is rendered for Transition itself
+          enter='transition ease-in-out duration-200'
+          enterFrom='transform opacity-0 scale-50'
+          enterTo='transform opacity-100 scale-100'
+          leave='transition ease-in duration-150'
+          leaveFrom='transform opacity-100 scale-100'
+          leaveTo='transform opacity-0 scale-50'
+        >
+          <MenuItems className='absolute left-0 origin-top z-10 overflow-auto
+            mt-2 max-h-[23rem] min-w-[15rem] max-w-[25rem] rounded-md shadow-black/40 dark:shadow-white/5
+            shadow-2xl ring-1 ring-(--border-color) bg-(--comp-bg-color) text-(--main-fg-color)
+            flex flex-col divide-y divide-(--border-color)/60'>
+              {tokens.map((token) => (
+                <MenuItem key={token.address}>
                   <div tabIndex={0}
-                       title={`Contract address: ${token.address}`}
-                       className='cursor-default px-3 py-2 rounded
-                            hover:bg-(--banner-bg-color) hover:text-blue-900'>
+                      title={`Contract address: ${token.address}`}
+                      className='cursor-default px-3 py-2 rounded
+                      hover:bg-(--banner-bg-color) hover:text-blue-900'>
                     <div className='flex flex-col'>
                       <div className='inline-flex items-center justify-between gap-[2rem]'>
                         <span className='font-semibold'>{token.symbol}</span>
@@ -84,9 +92,10 @@ export default function Tokens(props: {
                       <span>{token.name}</span>
                     </div>
                   </div>
-              </MenuItem>
-            ))}
-        </MenuItems>
+                </MenuItem>
+              ))}
+          </MenuItems>
+        </Transition>
       </Menu>
     );
   }
