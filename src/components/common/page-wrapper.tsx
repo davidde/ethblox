96d0722f4793417c829d5df41ccab5cc61b6e3ae
David Deprost <dadeprost@gmail.com>
2025-07-16 20:43:27 +0200

    Implement Dropdown Menu for Token list

--- src/components/common/page-wrapper.tsx
+++ src/components/common/page-wrapper.tsx
@@ -5,8 +5,8 @@ export default function PageWrapper({ children, maxWidthMd = '62rem' }: {
   return (
     <div className='flex items-center justify-center w-full px-[0.5rem] md:px-8'>
       <div className={`p-4 md:p-8 w-full max-w-[calc(100vw-1rem)] md:max-w-[${maxWidthMd}]
-         border border-(--border-color) bg-(--comp-bg-color) rounded-lg`}>
-      {children}
+          border border-(--border-color) bg-(--comp-bg-color) rounded-lg`}>
+        {children}
       </div>
     </div>
   );
--- src/components/content/address-page/tokens.tsx
+++ src/components/content/address-page/tokens.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 import ValueDisplay from '@/components/common/value-display';
 import { getAlchemy } from '@/lib/utilities';
 import { OwnedToken } from 'alchemy-sdk';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/16/solid';
 
 
 export default function Tokens(props: {
@@ -18,9 +20,9 @@ export default function Tokens(props: {
     if (props.network === 'mainnet') (async () => {
       try {
         const resp = await alchemy.core.getTokensForOwner(props.hash);
-        // Remove scam tokens; everything without logo/symbol or zero balance:
+        // Remove scam tokens; everything without name/logo/symbol or zero balance:
         let filteredTokens = resp.tokens.filter(token =>
-          token.logo && token.symbol && token.balance &&
+          token.name && token.logo && token.symbol && token.balance &&
           !(token.balance.startsWith('0') && token.balance.endsWith('0')));
         // Sort tokens alphabetically by symbol:
         filteredTokens.sort((a, b) => a.symbol!.localeCompare(b.symbol!));
@@ -33,14 +35,59 @@ export default function Tokens(props: {
     })();
   }, [alchemy, props.hash]);
 
-  function getTokenList(tokens: OwnedToken[] | undefined ) {
-    if (tokens && tokens.length === 0) return '/';
-    return tokens?.map((token, i) =>
-      <li key={i} className='ml-4 list-disc text-(--grey-fg-color)'>
-        <span className='text-(--main-fg-color)'>
-          {`${token.symbol}: ${parseFloat(token.balance!).toFixed(5)} (${token.name})`}
-        </span>
-      </li>
+  function getTokenList(tokens: OwnedToken[]) {
+    return tokens.map((token) => {
+      const bal = parseFloat(token.balance!);
+      const isZero = bal === 0;
+      let balance = bal.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 5 });
+      balance = !isZero && balance === '0.00' ? '~0.00' : balance;
+
+      return ({ // We can use non-null assertion since we filtered out null
+        name: token.name!, // and undefined values in useEffect() above!
+        symbol: token.symbol!,
+        balance: balance,
+        address: token.contractAddress,
+      })
+    });
+  }
+
+  function TokenDropdown(ownedTokens?: OwnedToken[]) {
+    if (!ownedTokens) return undefined;
+    if (ownedTokens && ownedTokens.length === 0) return '/';
+    let tokens = getTokenList(ownedTokens);
+
+    return (
+      <Menu as='div' className='relative inline-block text-left'>
+        <MenuButton className='relative w-[15rem] inline-flex items-center justify-between rounded-md
+            bg-(--card-bg-color) text-(--main-fg-color) px-3 py-1.5 text-sm/6 font-medium
+            border border-(--border-color) shadow-inner shadow-black/15 dark:shadow-white/10
+          hover:bg-(--banner-bg-color) data-open:bg-(--banner-bg-color)'>
+          {tokens.length} Tokens
+          <ChevronDownIcon className='size-4' />
+        </MenuButton>
+
+        <MenuItems className='absolute left-0 origin-top-left z-10 overflow-auto
+          mt-2 max-h-[23rem] min-w-[15rem] max-w-[25rem] rounded-md shadow-black/40 dark:shadow-white/5
+          shadow-2xl ring-1 ring-(--border-color) bg-(--comp-bg-color) text-(--main-fg-color)
+          flex flex-col divide-y divide-(--border-color)/60'>
+            {tokens.map((token) => (
+              <MenuItem key={token.address}>
+                  <div tabIndex={0}
+                       title={`Contract address: ${token.address}`}
+                       className='cursor-default px-3 py-2 rounded
+                            hover:bg-(--banner-bg-color) hover:text-blue-900'>
+                    <div className='flex flex-col'>
+                      <div className='inline-flex items-center justify-between gap-[2rem]'>
+                        <span className='font-semibold'>{token.symbol}</span>
+                        <span className='text-(--grey-fg-color) text-sm'>{token.balance}</span>
+                      </div>
+                      <span>{token.name}</span>
+                    </div>
+                  </div>
+              </MenuItem>
+            ))}
+        </MenuItems>
+      </Menu>
     );
   }
 
@@ -48,7 +95,7 @@ export default function Tokens(props: {
     <div className={props.network === 'mainnet' ? 'pr-12 my-4' : 'hidden'}>
       <h2 className='capsTitle'>TOKEN HOLDINGS</h2>
       <ValueDisplay
-        value={getTokenList(tokens)}
+        value={TokenDropdown(tokens)}
         error={tokensError}
         err='Error getting tokens'
       />
