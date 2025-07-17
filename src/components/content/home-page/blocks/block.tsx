26ae7c9237889834ea27fc8f272e16561c5308de
David Deprost <dadeprost@gmail.com>
2025-07-17 14:02:58 +0200

    Convert HomePage Block component to client component

--- src/components/content/home-page/blocks/block.tsx
+++ src/components/content/home-page/blocks/block.tsx
@@ -1,49 +1,61 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { CubeIcon } from '@heroicons/react/24/outline';
-import { Alchemy } from 'alchemy-sdk';
+import { type Block } from 'alchemy-sdk';
 import {
   getSecsFromUnixSecs,
   truncateAddress,
   getEtherValueFromWei,
-  getBlockRewardUrl
+  getBlockRewardUrl,
+  getAlchemy
 } from '@/lib/utilities';
 import Link from 'next/link';
 import PopoverLink from '../../../common/popover-link';
 
 
-type Props = {
+export default function Block(props: {
   blockNumber: number,
   network: string,
-  alchemy: Alchemy
-}
-
-export default async function Block(props: Props) {
+}) {
+  const alchemy = getAlchemy(props.network);
   const blockRewardUrl = getBlockRewardUrl(props.network, props.blockNumber);
-  let block;
-  let secsSinceAdded;
-  let blockReward;
-  let recipient, recipientShort;
 
-  if (props.blockNumber) {
-    try {
-      block = await props.alchemy.core.getBlock(props.blockNumber);
-    } catch(error) {
-      console.error('getBlock()', error);
-    }
-    try {
-      const response = await fetch(blockRewardUrl);
-      const data = await response.json();
-      blockReward = data.result?.blockReward;
-    } catch(error) {
-      console.error('getBlockReward()', error);
-    }
-  }
+  const [block, setBlock] = useState<Block>();
+  const [blockReward, setBlockReward] = useState('');
+  const [blockRewardError, setBlockRewardError] = useState('');
+  const [blockError, setBlockError] = useState('');
 
+  useEffect(() => {
+    if (props.blockNumber) (async () => {
+      try {
+        const resp = await alchemy.core.getBlock(props.blockNumber);
+        setBlock(resp);
+      } catch(err) {
+        const error = 'HomePage Block getBlock() ' + err;
+        console.error(error);
+        setBlockError(error);
+      }
+      try {
+        const resp = await fetch(blockRewardUrl);
+        if (!resp.ok) throw new Error(`Error: Response NOT OK, status: ${resp.status}`);
+        const data = await resp.json();
+        if (!data.result.blockReward) throw new Error('Error: Block reward missing from response.');
+        setBlockReward(`Ξ${getEtherValueFromWei(data.result.blockReward, 4)}`);
+      } catch(err) {
+        const error = 'HomePage Block getBlockReward() ' + err;
+        console.error(error);
+        setBlockRewardError(error);
+      }
+    })();
+  }, [alchemy, props.blockNumber, blockRewardUrl]);
+
+  let secsSinceAdded, recipient, recipientShort;
   if (block) {
     secsSinceAdded = getSecsFromUnixSecs(block.timestamp);
     recipient = block.miner;
     recipientShort = truncateAddress(recipient, 20);
   }
-  if (blockReward) blockReward = getEtherValueFromWei(blockReward, 4);
 
   return (
     <div className='p-2 md:p-3 border-b border-(--border-color) last:border-0'>
@@ -62,10 +74,11 @@ export default async function Block(props: Props) {
             <span className='md:pl-4 text-sm text-(--grey-fg-color)'>({secsSinceAdded} secs ago)</span>
           </div>
         </div>
+
         <div className='flex flex-col ml-12 md:ml-8 mb-2 md:mb-0'>
           <span className='px-2 md:px-4 leading-5'>{block?.transactions.length} transactions</span>
           <span className='pl-2 md:pl-4'>
-            Block Reward: { blockReward !== undefined && blockReward !== null ? `Ξ${blockReward}` : 'TBD' }
+            Block Reward: { blockReward !== undefined && blockReward !== null ? blockReward : 'TBD' }
           </span>
           <span className='pl-2 md:pl-4 leading-5'>
             Recipient:&nbsp;
--- src/components/content/home-page/blocks/index.tsx
+++ src/components/content/home-page/blocks/index.tsx
@@ -3,14 +3,14 @@ import Block from './block';
 import ErrorIndicator from '@/components/common/error-indicator';
 
 
-type Props = {
+export default async function Blocks(props: {
   blockNumber: number | undefined,
   network: string,
   alchemy: Alchemy
-}
+}) {
+  if (!props.blockNumber) return <ErrorIndicator error='Error getting latest block.' />;
 
-export default async function Blocks(props: Props) {
-  return (
+  else return (
     <div className='border border-(--border-color) bg-(--comp-bg-color)
                     rounded-lg w-full md:w-[48%] max-w-xl md:min-w-132 my-4 md:my-8 md:mr-12'>
       <h2 className='text-[1.15rem] font-bold p-2 pl-4 md:p-3 md:pl-4 border-b border-(--border-color)'>Latest Blocks</h2>
@@ -19,9 +19,8 @@ export default async function Blocks(props: Props) {
           [...Array(5)].map((x, i) =>
             <Block
               key={i}
-              blockNumber={props.blockNumber!-i-1}
+              blockNumber={props.blockNumber! - i - 1}
               network={props.network}
-              alchemy={props.alchemy}
             />
           )
           :
