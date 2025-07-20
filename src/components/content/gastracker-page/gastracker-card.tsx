dc42a897902fe624d541f62afe2bb4c061bda39d
David Deprost <dadeprost@gmail.com>
2025-07-20 20:28:56 +0200

    Update GastrackerPage with DataState ADT

--- src/components/content/gastracker-page/gastracker-card.tsx
+++ src/components/content/gastracker-page/gastracker-card.tsx
@@ -1,36 +1,52 @@
-import ValueDisplay from "@/components/common/value-display";
+import DataState from '@/lib/data-state';
+import { type Prices } from '../gastracker-page';
+import { getGasPriceGwei, getGasPriceUsd } from '@/lib/utilities';
 
 
-export default function GastrackerCard({
-  title,
-  gasPriceGwei,
-  gasPriceUsd,
-  priceError,
-  smiley,
-  smileyLabel,
-  colorClass,
-}: {
+export default function GastrackerCard({ title, prices }: {
   title: string,
-  gasPriceGwei?: string,
-  gasPriceUsd?: string,
-  priceError: any,
-  smiley?: string,
-  smileyLabel?: string,
-  colorClass?: string,
+  prices: DataState<Prices>,
 }) {
+  let gasPriceGwei, gasPriceUsd, smiley, smileyLabel, colorClass;
+  switch (title) {
+    case 'Low':
+      gasPriceGwei = () => getGasPriceGwei(prices.value!.lowGasPrice);
+      gasPriceUsd = () => getGasPriceUsd(prices.value!.lowGasPrice, prices.value!.ethPrice);
+      smiley = '😎';
+      smileyLabel = 'smiling face with sunglasses';
+      colorClass = 'text-green-600';
+      break;
+    case 'Average':
+      gasPriceGwei = () => getGasPriceGwei(prices.value!.averageGasPrice);
+      gasPriceUsd = () => getGasPriceUsd(prices.value!.averageGasPrice, prices.value!.ethPrice);
+      smiley = '😁';
+      smileyLabel = 'beaming face with smiling eyes';
+      colorClass = 'text-blue-600';
+      break;
+    case 'High':
+      gasPriceGwei = () => getGasPriceGwei(prices.value!.highGasPrice);
+      gasPriceUsd = () => getGasPriceUsd(prices.value!.highGasPrice, prices.value!.ethPrice);
+      smiley = '😵';
+      smileyLabel = 'face with crossed-out eyes';
+      colorClass = 'text-red-600';
+      break;
+  }
+
   return (
     <div className='w-58 px-16 py-4 my-2 ml-auto mr-auto bg-(--card-bg-color)
                     shadow-xl border border-(--border-color) rounded-lg'>
       <p className='font-bold mb-3'>
-        <span className='text-xl mr-2' role="img" aria-label={smileyLabel}>
+        <span className='text-xl mr-2' role='img' aria-label={smileyLabel}>
           {smiley}
         </span>
         {title}
       </p>
       <div className={`text-lg tracking-wide ${colorClass}`}>
-        <p><ValueDisplay value={gasPriceGwei} error={priceError} err='Error' /></p>
+        <p>
+          <prices.Render value={gasPriceGwei} error='Error' />
+        </p>
         <p className='text-sm'>
-          <ValueDisplay value={gasPriceUsd} error={priceError} err='Error' fallback={false} />
+          <prices.Render value={gasPriceUsd} showFallback={false} />
         </p>
       </div>
     </div>
--- src/components/content/gastracker-page/index.tsx
+++ src/components/content/gastracker-page/index.tsx
@@ -1,19 +1,22 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { getGasPriceGwei, getGasPriceUsd } from '@/lib/utilities';
 import { ChartBarIcon } from '@heroicons/react/24/outline';
 import GastrackerCard from './gastracker-card';
 import BreakMobile from '@/components/common/break-mobile';
 import PageWrapper from '@/components/common/page-wrapper';
+import DataState from '@/lib/data-state';
 
+
+export type Prices = {
+  ethPrice: number,
+  lowGasPrice: number,
+  averageGasPrice: number,
+  highGasPrice: number,
+};
 
 export default function GastrackerPage() {
-  const [ethPrice, setEthPrice] = useState<number>();
-  const [lowGasPrice, setLowGasPrice] = useState<number>();
-  const [averageGasPrice, setAverageGasPrice] = useState<number>();
-  const [highGasPrice, setHighGasPrice] = useState<number>();
-  const [priceError, setPriceError] = useState<string>();
+  const [prices, setPrices] = useState(DataState.value<Prices>());
 
   useEffect(() => {
     (async () => {
@@ -21,24 +24,17 @@ export default function GastrackerPage() {
         const res = await fetch('https://eth.blockscout.com/api/v2/stats');
         if (!res.ok) throw new Error(`Response NOT OK, status: ${res.status}`);
         const data = await res.json();
-        setEthPrice(+data.coin_price);
-        setLowGasPrice(+data.gas_prices.slow);
-        setAverageGasPrice(+data.gas_prices.average);
-        setHighGasPrice(+data.gas_prices.fast);
+        setPrices(DataState.value({
+          ethPrice: +data.coin_price,
+          lowGasPrice: +data.gas_prices.slow,
+          averageGasPrice: +data.gas_prices.average,
+          highGasPrice: +data.gas_prices.fast,
+        }));
       } catch(err) {
-        const error = 'GastrackerPage Price fetch ' + err;
-        console.error(error);
-        setPriceError(error);
+        setPrices(DataState.error(err));
       }
     })();
   }, []);
-
-  const lowGasPriceGwei = getGasPriceGwei(lowGasPrice);
-  const lowGasPriceUsd = getGasPriceUsd(lowGasPrice, ethPrice);
-  const averageGasPriceGwei = getGasPriceGwei(averageGasPrice);
-  const averageGasPriceUsd = getGasPriceUsd(averageGasPrice, ethPrice);
-  const highGasPriceGwei = getGasPriceGwei(highGasPrice);
-  const highGasPriceUsd = getGasPriceUsd(highGasPrice, ethPrice);
 
   return (
     <main>
@@ -58,33 +54,9 @@ export default function GastrackerPage() {
 
           <div className='flex flex-col md:flex-row items-start
             justify-between ml-auto mr-auto w-full max-w-200 my-10'>
-            <GastrackerCard
-              title='Low'
-              gasPriceGwei={lowGasPriceGwei}
-              gasPriceUsd={lowGasPriceUsd}
-              priceError={priceError}
-              smiley='😎'
-              smileyLabel='smiling face with sunglasses'
-              colorClass='text-green-600'
-            />
-            <GastrackerCard
-              title='Average'
-              gasPriceGwei={averageGasPriceGwei}
-              gasPriceUsd={averageGasPriceUsd}
-              priceError={priceError}
-              smiley='😁'
-              smileyLabel='beaming face with smiling eyes'
-              colorClass='text-blue-600'
-            />
-            <GastrackerCard
-              title='High'
-              gasPriceGwei={highGasPriceGwei}
-              gasPriceUsd={highGasPriceUsd}
-              priceError={priceError}
-              smiley='😵'
-              smileyLabel='face with crossed-out eyes'
-              colorClass='text-red-600'
-            />
+            <GastrackerCard title='Low' prices={prices} />
+            <GastrackerCard title='Average' prices={prices} />
+            <GastrackerCard title='High' prices={prices} />
           </div>
         </div>
       </PageWrapper >
