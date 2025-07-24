18ab902b4033d7034454504410f641a9fd54d203
David Deprost <dadeprost@gmail.com>
2025-07-24 20:17:20 +0200

    Move getBlockReward() fetch to its own component to prevent timestamp update;
    Usually more up to date data would be good, but in this case it is very confusing.
    When refetching a block reward on error (after user clicks its refresh icon),
    the timestamp updates along, resulting in this block looking older than possibly
    more recent blocks, since they were not updated and are still showing the timestamp
    from their original fetch.
    Moving the fetch to its own component prevents the getBlockReward() refetch from
    also reactivating the getBlock() fetch. This also results in better separation of concerns
    by giving both components a single responsibility / data fetch.

---
+++ src/components/content/home-page/blocks/block-reward.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import DataState from '@/lib/data-state';
+import LoadingPulse from '@/components/common/indicators/loading-pulse';
+import LoadingPulseStatic from '@/components/common/indicators/loading-pulse-static';
+import ErrorWithRetry from '@/components/common/indicators/error-with-retry';
+import { getBlockRewardUrl, getEtherValueFromWei } from '@/lib/utilities';
+
+
+export default function BlockReward(props: {
+  id: number,
+  network: string,
+  blockNumber?: number,
+}) {
+  const [blockReward, setBlockReward] = useState(DataState.value<string>());
+
+  async function getBlockReward() {
+    if (props.blockNumber) try {
+      const resp = await fetch(getBlockRewardUrl(props.network, props.blockNumber));
+      if (!resp.ok) throw new Error(`Response NOT OK, status: ${resp.status}`);
+      const data = await resp.json();
+      if (!data.result || !data.result.blockReward) {
+        // Latest Block often doesn't have reward yet:
+        if (props.id === 0) setBlockReward(DataState.value('TBD'));
+        else throw new Error('Block reward missing from response.');
+      }
+      else setBlockReward(DataState.value(`Ξ${getEtherValueFromWei(data.result.blockReward, 4)}`));
+    } catch(err) {
+      setBlockReward(DataState.error(err));
+    }
+  }
+
+  useEffect(() => {
+    getBlockReward();
+  }, [props.blockNumber, props.id, props.network]);
+
+  return (
+    <span className='pl-2 md:pl-4'>
+      <LoadingPulseStatic
+        content='Block Reward:'
+        dataState={blockReward}
+        className='bg-(--grey-fg-color)'
+      />
+      &nbsp;&nbsp;
+      <blockReward.Render
+        loadingFallback={<LoadingPulse className='bg-(--grey-fg-color) w-[4rem]' />}
+        errorFallback={<ErrorWithRetry retry={getBlockReward} />}
+      />
+    </span>
+  );
+}
--- src/components/content/home-page/blocks/block.tsx
+++ src/components/content/home-page/blocks/block.tsx
@@ -7,8 +7,6 @@ import {
   getSecsFromUnixSecs,
   getBlockAgeFromSecs,
   truncateAddress,
-  getEtherValueFromWei,
-  getBlockRewardUrl,
   getAlchemy,
 } from '@/lib/utilities';
 import DataState from '@/lib/data-state';
@@ -17,6 +15,7 @@ import PopoverLink from '../../../common/popover-link';
 import LoadingPulse from '@/components/common/indicators/loading-pulse';
 import LoadingPulseStatic from '@/components/common/indicators/loading-pulse-static';
 import ErrorWithRetry from '@/components/common/indicators/error-with-retry';
+import BlockReward from './block-reward';
 
 
 export default function Block(props: {
@@ -26,9 +25,7 @@ export default function Block(props: {
 }) {
   const alchemy = getAlchemy(props.network);
   const blockNumber = props.latestBlockData.value ? props.latestBlockData.value - props.id : undefined;
-
   const [block, setBlock] = useState(DataState.value<Block>());
-  const [blockReward, setBlockReward] = useState(DataState.value<string>());
 
   async function getBlock() {
     if (blockNumber) try {
@@ -39,25 +36,8 @@ export default function Block(props: {
     }
   }
 
-  async function getBlockReward() {
-    if (blockNumber) try {
-      const resp = await fetch(getBlockRewardUrl(props.network, blockNumber));
-      if (!resp.ok) throw new Error(`Response NOT OK, status: ${resp.status}`);
-      const data = await resp.json();
-      if (!data.result || !data.result.blockReward) {
-        // Latest Block often doesn't have reward yet:
-        if (props.id === 0) setBlockReward(DataState.value('TBD'));
-        else throw new Error('Block reward missing from response.');
-      }
-      else setBlockReward(DataState.value(`Ξ${getEtherValueFromWei(data.result.blockReward, 4)}`));
-    } catch(err) {
-      setBlockReward(DataState.error(err));
-    }
-  }
-
   useEffect(() => {
     getBlock();
-    getBlockReward();
   }, [alchemy, blockNumber, props.id, props.network]);
 
   return (
@@ -93,18 +73,11 @@ export default function Block(props: {
               errorFallback={<ErrorWithRetry retry={getBlock} />}
             />
           </span>
-          <span className='pl-2 md:pl-4'>
-            <LoadingPulseStatic
-              content='Block Reward:'
-              dataState={blockReward}
-              className='bg-(--grey-fg-color)'
-            />
-            &nbsp;&nbsp;
-            <blockReward.Render
-              loadingFallback={<LoadingPulse className='bg-(--grey-fg-color) w-[4rem]' />}
-              errorFallback={<ErrorWithRetry retry={getBlockReward} />}
-            />
-          </span>
+          <BlockReward
+            id={props.id}
+            network={props.network}
+            blockNumber={blockNumber}
+          />
           <span className='pl-2 md:pl-4 leading-5'>
             <LoadingPulseStatic
               content='Recipient:'
