0c41908e522d714ef9dde904cd51e2d27c824d66
David Deprost <dadeprost@gmail.com>
2025-08-07 14:56:15 +0200

    Refactor NodeBanner

---
+++ src/components/content/home-page/node-banner/index.tsx
@@ -0,0 +1,257 @@
+'use client';
+
+import { useRef, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+
+
+export default function NodeBanner(props: { className?: string }) {
+  // Settings:
+  const colored = false; // gives nodes random colors when true, nodeColor when false
+  const nodeSize = 3; // medium node radius in pixels
+  const lineWidth = 2; // node connection line width in pixels
+  const speedMultiplier = 0.2; // speed multiplier
+  const nodeAmountMax = 100; // node amount on desktop, will be less on mobile; the more the slower
+  const drawLineThresholdMax = 100; // distance threshold for drawing the lines between nodes on desktop; higher = more lines = slower
+  const { resolvedTheme } = useTheme(); // theme is required to update NodeBanner colors on theme switch
+  // Canvas ref is null before render:
+  const canvas = useRef<HTMLCanvasElement>(null);
+
+  // Variables to be set by methods:
+  let context: CanvasRenderingContext2D;
+  let theme;
+  let nodes: Node[] = []; // node array
+  let width: number; // node canvas width in pixels
+  let height: number; // node canvas height in pixels
+  let nodeAmount: number;
+  let drawLineThreshold: number;
+  let speed: number;
+  let bgColor: string, nodeColor: string, lineColor: string;
+  let linearGradient: CanvasGradient, linearGradientToColor: string;
+  let radialGradient: CanvasGradient, radialGradientTransparency: string;
+
+  useEffect(() => {
+    // Canvas ref is null before render:
+    if (!canvas.current) return;
+    context = canvas.current.getContext('2d')!;
+
+    // This conditional fixes the CSS vars update on theme switch;
+    // when getting the CSS vars directly from document.documentElement,
+    // it uses the CSS vars of the old theme that was switched away from!
+    theme = resolvedTheme === 'light' ? document.querySelector('.light')! : document.querySelector('.dark')!;
+    bgColor = window.getComputedStyle(theme).getPropertyValue('--banner-bg-color')
+    nodeColor = window.getComputedStyle(theme).getPropertyValue('--banner-node-color');
+    lineColor = window.getComputedStyle(theme).getPropertyValue('--banner-line-color');
+    linearGradientToColor = window.getComputedStyle(theme).getPropertyValue('--banner-linear-gradient-to-color');
+    // Alpha of bright center circle of radial gradient:
+    radialGradientTransparency = resolvedTheme === 'light' ? '1' : '0.13';
+
+    setupCanvasData(); // Set up canvas
+
+    // Create linear gradient for canvas background:
+    linearGradient = context.createLinearGradient(0, 0, 0, height);
+    // Create radial gradient for canvas background:
+    radialGradient = context.createRadialGradient(
+      width / 3.1, // x0: x position of inner circle’s center (focus point)
+      height / 2.6, // y0: y position of inner circle’s center (focus point)
+      0, // r0: Radius of the inner circle (start radius)
+      width / 3.1, // x1: x position of outer circle’s center
+      height / 2.6,// y1: y position of the outer circle’s center
+      width / 5  // r1: Radius of the outer circle (end radius)
+    );
+
+    requestAnimationFrame(loop); // Set up loop
+
+    // Resize event listener:
+    window.addEventListener('resize', setupCanvasData);
+    return () => window.removeEventListener('resize', setupCanvasData);
+  }, [resolvedTheme, colored])
+
+  // Node class and constructor:
+  class Node {
+    x: number; y: number; id: number;
+    speedX: number; speedY: number;
+    size: number;
+    color: string;
+    move: () => void;
+
+    constructor(x: number, y: number, id: number) {
+      this.x = x;
+      this.y = y;
+      this.id = id;
+
+      // If the node id is even, start moving them in the opposite direction:
+      if (id % 2) {
+        this.speedX = randomInt(1, 100)/100 * speed;
+        this.speedY = randomInt(1, 100)/100 * speed;
+      } else {
+        this.speedX = randomInt(1, 100)/100 * speed * -1;
+        this.speedY = randomInt(1, 100)/100 * speed * -1;
+      }
+
+      // Create some variation in node sizes:
+      this.size = this.id > Math.floor(nodeAmount * 0.75) ? nodeSize + 1.5 :
+                  this.id <= Math.floor(nodeAmount * 0.25) ? nodeSize - 1 : nodeSize;
+
+      // Color array for random color setting:
+      const colors = ['#4f91f9', '#a7f94f', '#f94f4f', '#f9f74f', '#8930ff', '#fc4edf', '#ff9c51'];
+      // Set random color from array if colored is true:
+      this.color = colored ? colors[Math.floor(Math.random() * colors.length)] : nodeColor;
+
+      // Move the nodes and draw them:
+      this.move = function() {
+        this.y += this.speedY;
+        this.x += this.speedX;
+
+        // Make them bounce:
+        if (this.y < this.size || this.y > height - this.size) {
+          this.speedY = - this.speedY;
+        }
+        if (this.x < this.size || this.x > width - this.size) {
+          this.speedX = - this.speedX;
+        }
+
+        // Draw the nodes:
+        context.fillStyle = this.color;
+        circle(this.x, this.y, this.size);
+        context.fill();
+      };
+    }
+  }
+
+  // Set up all canvas and node data:
+  function setupCanvasData() {
+    width = window.innerWidth; // Set node canvas width
+    let isMobile = width <= 768; // Tailwind `md:` = `@media (width >= 48rem)` = 768px
+    nodeAmount = isMobile ? Math.floor(0.4 * nodeAmountMax) : nodeAmountMax;
+    drawLineThreshold = isMobile ? 0.9 * drawLineThresholdMax : drawLineThresholdMax;
+    speed = isMobile ? 0.7 * speedMultiplier : speedMultiplier;
+    height = Number(window.getComputedStyle(document.body).getPropertyValue('--node-banner-height'));
+    canvas.current!.width = width;
+    canvas.current!.height = height;
+    nodes = []; // Reset the nodes for resize event listener
+
+    // Generate 'nodeAmount' number of nodes with random positions and push them to the nodes array:
+    for (var i = 0; i < nodeAmount; i++) {
+      var node = new Node(randomInt(0, width), randomInt(0, height), i);
+      nodes.push(node);
+    }
+  }
+
+  // Loop function to animate the canvas:
+  function loop() {
+    // Clear the entire canvas every frame:
+    context.clearRect(0, 0, width, height); 
+
+    // Redraw the linear gradient background every frame:
+    // (Before nodes and lines, so they appear on top of it)
+    linearGradient.addColorStop(0.0, bgColor);
+    linearGradient.addColorStop(0.8, bgColor); // Start color transition at 80%
+    linearGradient.addColorStop(1.0, linearGradientToColor);
+    context.fillStyle = linearGradient;
+    context.fillRect(0, 0, width, height);
+
+    // Move all nodes:
+    for (let i = 0; i < nodes.length; i++) nodes[i].move();
+    // Draw the lines again:
+    drawLines();
+
+    // Redraw the circle gradient background every frame:
+    // (After nodes and lines, so they appear under it)
+    radialGradient.addColorStop(0, `rgba(255, 255, 255, ${radialGradientTransparency})`); // Bright center
+    radialGradient.addColorStop(1, 'rgba(255, 255, 255, 0)'); // Fade to transparent
+    context.fillStyle = radialGradient;
+    context.fillRect(0, 0, width, height);
+
+    // Repeat the loop:
+    requestAnimationFrame(loop);
+  }
+
+  // Draw the lines between the nodes:
+  function drawLines() {
+    // Get the rgb values for the color of the line:
+    let rgbValues = lineColor.startsWith('rgb') ? rgbToRgbNums(lineColor) : hexToRgbNums(lineColor);
+
+    for (let i = 0; i < nodes.length; i++) {
+      if (colored) rgbValues = nodes[i].color.startsWith('rgb') ? rgbToRgbNums(nodes[i].color) : hexToRgbNums(nodes[i].color);
+
+      // Get the origin point:
+      let x1 = nodes[i].x;
+      let y1 = nodes[i].y;
+
+      // Get the destination point in a subloop that only loops the remaining nodes, to avoid duplicate lines:
+      for (let j = i+1; j < nodes.length; j++) {
+        // Get the destination point:
+        let x2 = nodes[j].x;
+        let y2 = nodes[j].y;
+
+        // Calculate the distance between the origin and target points:
+        let dist = distance(x1, x2, y1, y2);
+
+        // If the distance is less than the threshold, draw the line:
+        if (dist < drawLineThreshold) {
+          let finalOpacity = 1 - (dist / drawLineThreshold);
+          context.strokeStyle = `rgba(${rgbValues!.r}, ${rgbValues!.g}, ${rgbValues!.b}, ${finalOpacity})`;
+          line(x1, y1, x2, y2);
+        }
+      }
+    }
+  }
+
+  // Draw line given a set of two points (as x1, y1, x2 and y2):
+  function line(x1: number, y1: number, x2: number, y2: number) {
+    context.lineWidth = lineWidth;
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+  }
+
+  // Draw circle given x, y and radius:
+  const circle = (x: number, y: number, radius: number) => {
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.closePath();
+  }
+
+  // Return random int from min to max inclusive:
+  function randomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  // Calculate the distance between two points:
+  function distance(x1: number, x2: number, y1: number, y2: number) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  }
+
+  // Convert from hex string to rgb number:
+  function hexToRgbNums(hex: string) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  // Convert from rgb() string to rgb number:
+  function rgbToRgbNums(rgb: string) {
+    var result = /^rgb\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)$/i.exec(rgb);
+    return result ? {
+        r: parseInt(result[1]),
+        g: parseInt(result[2]),
+        b: parseInt(result[3])
+    } : null;
+  }
+
+  return (
+    <div className={`${props.className} w-full bg-(--banner-bg-color)
+                dark:shadow-xl/10 dark:shadow-[#3a3a3a]
+                h-(--node-banner-height-px) -mt-(--content-y-margin)`}>
+      {/* The following 2 spans are required to make the conditional
+          `window.getComputedStyle()` on line 90 work properly: */}
+      <span className='hidden light' />
+      <span className='hidden dark' />
+      <canvas ref={canvas} />
+    </div>
+  );
+}
